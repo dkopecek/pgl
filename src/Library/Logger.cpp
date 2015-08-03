a89ad5101a7a97aa0f2d289f06eb3e12bf6ac354
Daniel Kopecek <dkopecek@redhat.com>
2015-08-03 16:31:06 +0200

    Implement a simple logger for debugging purposes

---
+++ src/Library/Logger.cpp
@@ -0,0 +1,112 @@
+//
+// Copyright (C) 2015 Red Hat, Inc.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Authors: Daniel Kopecek <dkopecek@redhat.com>
+//
+#include "Logger.hpp"
+#include <sys/types.h>
+#include <unistd.h>
+
+namespace pgl
+{
+  /* Instantiate the logger */
+  Logger G_logger;
+
+  LogStream::LogStream(Logger& logger, const std::string& file, int line,
+      const std::string& function)
+    : _logger(logger), _file(file), _line(line), _function(function)
+  {
+  }
+
+  LogStream::LogStream(const LogStream& rhs)
+    : _logger(rhs._logger), _file(rhs._file), _line(rhs._line),
+    _function(rhs._function)
+  {
+  }
+
+  LogStream::~LogStream()
+  {
+    _logger.write(_file, _line, _function, str());
+  }
+
+  Logger::Logger()
+  {
+    const char * const envval = getenv("PGL_DEBUG");
+    _enabled = false;
+    /*
+     * If PGL_DEBUG=1 is set in the current environment, then
+     * open the log file stream and mark the logger as active.
+     */
+    if (envval != nullptr && envval[0] == '1') {
+      _path = "pgl-debug.";
+      _path += std::to_string(getpid());
+      _path += ".log";
+      _stream.open(_path);
+      _enabled = true;
+    }
+  }
+
+  Logger::~Logger()
+  {
+    if (isEnabled()) {
+      _stream.close();
+      _enabled = false;
+    }
+  }
+
+  bool Logger::isEnabled() const
+  {
+    return _enabled;
+  }
+
+  LogStream Logger::operator()(const std::string& file, const int line,
+      const std::string& function)
+  {
+    return LogStream(*this, file, line, function);
+  }
+
+  void Logger::write(const std::string& file, int line, const std::string& func,
+      const std::string& message)
+  {
+    std::unique_lock<std::mutex> lock(_mutex);
+
+    _stream << timestamp() << " "
+      << file << " "
+      << line << " "
+      << func << " "
+      << ": " << message << std::endl;
+
+    return;
+  }
+
+  /*
+   * Generate a timestamp string in the form:
+   * <seconds>.<microseconds>
+   */
+  const std::string Logger::timestamp()
+  {
+    struct timeval tv_now = { 0, 0 };
+
+    if (gettimeofday(&tv_now, nullptr) != 0) {
+      throw SyscallError("gettimeofday", errno);
+    }
+
+    return std::to_string(tv_now.tv_sec) \
+      + "." + std::to_string(tv_now.tv_usec);
+  }
+
+} /* namespace pgl */
+
---
+++ src/Library/Logger.hpp
@@ -0,0 +1,78 @@
+//
+// Copyright (C) 2015 Red Hat, Inc.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Authors: Daniel Kopecek <dkopecek@redhat.com>
+//
+#pragma once
+#include "Exceptions.hpp"
+#include <mutex>
+#include <fstream>
+#include <sstream>
+#include <sys/time.h>
+
+namespace pgl
+{
+  class Logger;
+
+  class LogStream : public std::ostringstream
+  {
+    public:
+      LogStream(Logger& logger, const std::string& file, int line,
+          const std::string& function);
+      LogStream(const LogStream& rhs);
+      ~LogStream();
+
+    private:
+      Logger& _logger;
+      const std::string& _file;
+      const int _line;
+      const std::string& _function;
+  };
+
+  class Logger
+  {
+    public:
+      Logger();
+      ~Logger();
+
+      bool isEnabled() const;
+
+      LogStream operator()(const std::string& file, const int line,
+          const std::string& function);
+
+      void write(const std::string& file, int line, const std::string& func,
+          const std::string& message);
+
+      /*
+       * Generate a timestamp string in the form:
+       * <seconds>.<microseconds>
+       */
+      static const std::string timestamp();
+
+    private:
+      bool _enabled;
+      std::string _path;
+      std::mutex _mutex;
+      std::ofstream _stream;
+  };
+
+  extern Logger G_logger;
+
+#define PGL_LOG() \
+  if (G_logger.isEnabled()) \
+  G_logger(__FILE__, __LINE__, __PRETTY_FUNCTION__)
+
+} /* namespace pgl */
